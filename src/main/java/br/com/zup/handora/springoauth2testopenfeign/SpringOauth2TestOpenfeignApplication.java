cf7c2860e2d68f528e10ea7ea75d120a5583b6e9
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-07-12 15:21:03 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/handora/springoauth2testopenfeign/SpringOauth2TestOpenfeignApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.springoauth2testopenfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2TestOpenfeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2TestOpenfeignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zup/handora/springoauth2testopenfeign/SpringOauth2TestOpenfeignApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.zup.handora.springoauth2testopenfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2TestOpenfeignApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
